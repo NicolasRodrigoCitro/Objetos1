b5ab9ecf38d28cf9006483294437dd55b9f1ecdc
Nicolas Citro <NicolasRodrigoCitro@gmail.com>
2023-09-17 22:31:57 -0300

    actualizaciones del ejer 6

--- objetos1/Ejercicio6RedDeAlumbrado/src/main/java/Ejercicio5Mamifero/Ejercicio6RedDeAlumbrado/Farola.java
+++ objetos1/Ejercicio6RedDeAlumbrado/src/main/java/Ejercicio5Mamifero/Ejercicio6RedDeAlumbrado/Farola.java
@@ -1,46 +1,57 @@
 package Ejercicio5Mamifero.Ejercicio6RedDeAlumbrado;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.function.BooleanSupplier;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Farola {
 	private boolean encendido;
-	private Set<Farola> farolas;
-	
+	private List<Farola> farolas;
+
 	public Farola() {
-		this.encendido=false;
-		this.farolas= new HashSet<>();
+		this.encendido = false;
+		this.farolas = new ArrayList<>();
 	}
+
 	public boolean isOn() {
 		return this.encendido;
 	}
+
 	public Object getNeighbors() {
-		return new HashSet<>(this.farolas);
+		return new ArrayList<>(this.farolas);
 	}
+
 	private boolean comprobarVecino(Farola farolaDos) {
-		//retorna si la farola recibida por parametro ya existe en la coleccion.
-		return this.farolas.equals(farolaDos);
+		// retorna si la farola recibida por parametro ya existe en la coleccion.
+		//return this.farolas.equals(farolaDos);
+		return this.farolas.stream().filter(f -> f.equals(farolaDos)).findFirst();
 	}
+
 	public void pairWithNeighbor(Farola farolaDos) {
-		if(!comprobarVecino(farolaDos)) {
+		if (!comprobarVecino(farolaDos)) {
 			this.farolas.add(farolaDos);
 			farolaDos.pairWithNeighbor(this);
 		}
-				
+
 	}
+// consultar con el ayudante
 	public void turnOn() {
-		if(!this.encendido) {
-			this.encendido=true;
-			this.farolas.stream().turnOn();
-			
+		if (!this.encendido) {
+			this.encendido = true;
+			for (Farola f: this.farolas) {
+				f.turnOn();
+			}
 		}
-		
+
 	}
+
 	public void turnOff() {
-		// TODO Auto-generated method stub
-		
+		if (this.encendido) {
+			this.encendido = true;
+			for (Farola f: this.farolas) {
+				f.turnOff();
+			}
+		}
+
 	}
-	
-	
+
 }
